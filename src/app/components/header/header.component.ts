8dd9ea671a33717b65488878fa04559856502832
gustavorsan <gustavorsantos42@gmail.com>
2022-12-18 21:08:12 -0300

    resolução ex02

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component,Output,EventEmitter } from '@angular/core';
+
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  @Output() emitPage = new EventEmitter<string>();
+
+  sendPage(value: string) {
+    this.emitPage.emit(value);
+  }
+}
